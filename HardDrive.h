17ce965ade83eea653c443981691a686ac203080
Mo_Fares <94139202+MoFares7@users.noreply.github.com>
2022-01-02 00:51:14 +0200

    Add files via upload

---
+++ HardDrive.h
@@ -0,0 +1,25 @@
+
+#pragma once
+class HardDrive
+{
+public:
+	HardDrive();
+	void HardDrive::drawModel();
+
+
+
+private:
+	void QuadsCollected1();
+	void  QuadsCollected2();
+	void QuadsCpu1();
+	void QuadsCpu2();
+	void QuadsRam1();
+	void QuadsRam2();
+	void QuadsIc1();
+	void QuadsIc2();
+	void QuadsCapacitor1();
+	void QuadsCapacitor2();
+
+
+
+};
